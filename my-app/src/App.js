ebbd279993d2996ac227619d1d8fddc3eba7d681
Harikrishnan-HK <krishnanhari@outlook.com>
2024-01-31 08:56:47 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,47 @@
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text:
+      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text:
+      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
+  }
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({data}) {
+  return (
+  <div className="accordian">
+    {data.map((e,i)=> <AccordionItem title={e.title} text={e.text} num={i}/>)}
+  </div>
+  );
+}
+
+function AccordionItem({num, title, text}) {
+  return (
+  <div className="item">
+    <p className="number">{num}</p>
+    <p className="title">{title}</p>
+    <p className="icon">-</p>
+    <p className="text">{text}</p>
+
+  </div>
+);
+}
